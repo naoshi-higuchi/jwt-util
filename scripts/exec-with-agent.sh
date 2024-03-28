debc28b90b2c24a1dc4cfce5455072a17d1e6e29
Naoshi HIGUCHI <naoshi.higuchi@gmail.com>
2024-03-28 21:26:41 +0900

    fix: native-image configuration was not updated.

--- scripts/exec-with-agent.sh
+++ scripts/exec-with-agent.sh
@@ -2,5 +2,31 @@
 
 JAR=../target/jwt-util.jar
 OUTPUTDIR=../src/main/resources/META-INF/native-image
-java -agentlib:native-image-agent=config-output-dir=${OUTPUTDIR} -jar ${JAR} encode ./claims.json --algorithm RS256 --key ./rsa-private.pem > ./jwt
-#java -agentlib:native-image-agent=config-output-dir=${OUTPUTDIR} -jar ${JAR} decode ./jwt
+
+for hashLen in 256 384 512; do
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} encode ./claims.json --algorithm RS${hashLen} --key ./rsa-private.pem > ./jwt
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} decode ./jwt
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} verify --key ./rsa-public.pem ./jwt
+done
+
+for hashLen in 256 384 512; do
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} encode ./claims.json --algorithm PS${hashLen} --key ./rsa-pss-${hashLen}-private.pem > ./jwt
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} decode ./jwt
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} verify --key ./rsa-pss-${hashLen}-public.pem ./jwt
+done
+
+for hashLen in 256 384; do
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} encode ./claims.json --algorithm ES${hashLen} --key ./ec${hashLen}-key-pair.pem > ./jwt
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} decode ./jwt
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} verify --key ./ec${hashLen}-public.pem ./jwt
+done
+java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} encode ./claims.json --algorithm ES512 --key ./ec521-key-pair.pem > ./jwt
+java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} decode ./jwt
+java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} verify --key ./ec521-public.pem ./jwt
+
+
+for hashLen in 256 384 512; do
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} encode ./claims.json --algorithm HS${hashLen} --key ./secret-hs${hashLen}.bin > ./jwt
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} decode ./jwt
+  java -agentlib:native-image-agent=config-merge-dir=${OUTPUTDIR} -jar ${JAR} verify --key ./secret-hs${hashLen}.bin ./jwt
+done
